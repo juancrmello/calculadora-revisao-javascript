98a7e0f09a9aec1e52fb048ca5e4d8a0da43c77b
Juan Mello <juanribeirofx@gmail.com>
2021-06-08 23:33:47 -0300

    Adicionando o evento de click

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -8,6 +8,7 @@ class CalcController {
         this._timeEl = document.querySelector("#hora");
         this._currentDate;
         this.initialize();
+        this.initButtonsEvents();
     }
 
     initialize() {
@@ -21,7 +22,15 @@ class CalcController {
     }
 
     initButtonsEvents() {
-        let buttons = document.querySelectorAll("#buttons >g, #parts > g");
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+
+        buttons.forEach((btn, index)=> {
+
+            btn.addEventListener('click', e=> {
+                console.log(btn.className.baseVal.replace('btn-', ""));
+            })
+        });
+
     }
 
     setDisplayDateTime() {
