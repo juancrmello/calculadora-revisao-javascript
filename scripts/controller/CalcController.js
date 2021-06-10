682e270c725add84b74fe8541979aa7825d6c827
Juan Mello <juanribeirofx@gmail.com>
2021-06-09 23:00:30 -0300

    Botao Ponto

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -35,6 +35,8 @@ class CalcController {
     clearAll() {
 
         this._operation = [];
+        this._lastOperator = '';
+        this._lastNumber = '';
 
         this.setLastNumberToDisplay();
 
@@ -169,10 +171,6 @@ class CalcController {
 
                 this.setLastOperation(value);
 
-            } else if(isNaN(value)) {
-
-                console.log('Caiu aqui', value);
-
             } else {
 
                 this.pushOperation(value);
@@ -191,7 +189,7 @@ class CalcController {
 
                 let newValue = this.getLastOperation().toString() + value.toString();
                 
-                this.setLastOperation(parseInt(newValue));
+                this.setLastOperation(newValue);
 
                 this.setLastNumberToDisplay();
 
@@ -203,6 +201,22 @@ class CalcController {
 
     setError() {
         this.displayCalc = "Error";
+    }
+
+    addDot() {
+
+        let lastOperation = this.getLastOperation();
+
+        if (typeof lastOperation === 'string' && lastOperation.split('').indexOf('.') > -1) return;
+
+        if (this.isOperator(lastOperation) || !lastOperation) {
+            this.pushOperation('0.');
+        } else {
+            this.setLastOperation(lastOperation.toString() + '.');
+        }
+
+        this.setLastNumberToDisplay();
+
     }
 
     execBtn(value) {
@@ -242,7 +256,7 @@ class CalcController {
                 break;
             
             case 'ponto':
-                this.addOperation('.');
+                this.addDot('.');
                 break;
             
             case '0':
